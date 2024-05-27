498ad76656b10309a927d90f9bb90d20b78d0f8a
SBern1w <marchesin.federico@gmail.com>
2024-05-27 14:22:56 +0200

    new runs

---
+++ 20240507_traking_unitary_efficent/20240525_1_run/code/photonic_components/Crossing_layers.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+
+
+# Select GPU if available, else fall back to CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+class CrossingLayerMatrix_Odd(nn.Module):
+    r"""
+    Create odd Crossings matrix
+        0__   __0
+           \-/
+        1__   __1
+           \-/
+        2__/-\__2
+        
+        3__/-\__3
+
+    Where:
+        0__   __0                              |  sqrt(CT)  | sqrt(1-CT) |
+           \-/        =>   TF = sqrt(1-LOSS) x |            |            |
+        1__/-\__1                              | sqrt(1-CT) |  sqrt(CT)  |
+    """
+    def __init__(self,
+                 n_inputs: int,
+                 crossing_i_losses: torch.Tensor = None,
+                 crossing_crosstalks: torch.Tensor = None):
+        super(CrossingLayerMatrix_Odd, self).__init__()
+        if n_inputs%2 == 1: raise Exception('n_inputs is odd!!! NONONO, put it even!!!')
+        _n_crossing = n_inputs//2 - 1
+        if crossing_i_losses is None or crossing_crosstalks is None:
+            atten = torch.ones(_n_crossing, device=device)
+            tau = torch.zeros(_n_crossing, device=device)
+            kappa = atten
+            edge_up_crossing = torch.Tensor([1.], device=device)
+            edge_dw_crossing = edge_up_crossing
+        else:
+            atten = 1 - crossing_i_losses[1:-1]
+            tau = crossing_crosstalks[1:-1]
+            kappa = 1 - crossing_crosstalks[1:-1]
+            edge_up_crossing = torch.sqrt(1 - crossing_i_losses[0]) * torch.sqrt(1 - crossing_crosstalks[0])
+            edge_dw_crossing = torch.sqrt(1 - crossing_i_losses[-1]) * torch.sqrt(1 - crossing_crosstalks[-1])
+
+        tau_tf = torch.sqrt(atten) * torch.sqrt(tau).to(torch.complex64)
+        kappa_tf = torch.sqrt(atten) * torch.sqrt(kappa).to(torch.complex64)
+        
+        crossing_elements = torch.stack([
+            torch.stack([tau_tf, kappa_tf], dim=-1),
+            torch.stack([kappa_tf, tau_tf], dim=-1)
+            ], dim=-2)
+
+        self._crossing_tf_matrix = torch.block_diag(edge_up_crossing, *crossing_elements, edge_dw_crossing)
+    
+    def forward(self, x_matrix):
+        return torch.mm(self._crossing_tf_matrix, x_matrix)
+
+
+class CrossingLayerMatrix_Odd_noTopBottom(nn.Module):
+    r"""
+    Create odd Crossings matrix
+        0_______0
+        
+        1__   __1
+           \-/
+        2__/-\__2
+        
+        3_______3
+
+    Where:
+        0__   __0                              |  sqrt(CT)  | sqrt(1-CT) |
+           \-/        =>   TF = sqrt(1-LOSS) x |            |            |
+        1__/-\__1                              | sqrt(1-CT) |  sqrt(CT)  |
+    """
+    def __init__(self,
+                 n_inputs: int,
+                 crossing_i_losses: torch.Tensor = None,
+                 crossing_crosstalks: torch.Tensor = None):
+        super(CrossingLayerMatrix_Odd_noTopBottom, self).__init__()
+        if n_inputs%2 == 1: raise Exception('n_inputs is odd!!! NONONO, put it even!!!')
+        _n_crossing = n_inputs//2 - 1
+        if crossing_i_losses is None or crossing_crosstalks is None:
+            atten = torch.ones(_n_crossing, device=device)
+            tau = torch.zeros(_n_crossing, device=device)
+            kappa = atten
+            edge_up_crossing = torch.Tensor([1.], device=device)
+            edge_dw_crossing = edge_up_crossing
+        else:
+            atten = 1 - crossing_i_losses
+            tau = crossing_crosstalks
+            kappa = 1 - crossing_crosstalks
+            edge_up_crossing = torch.Tensor([1.], device=device)
+            edge_dw_crossing = edge_up_crossing
+
+        tau_tf = torch.sqrt(atten) * torch.sqrt(tau).to(torch.complex64)
+        kappa_tf = torch.sqrt(atten) * torch.sqrt(kappa).to(torch.complex64)
+        
+        crossing_elements = torch.stack([
+            torch.stack([tau_tf, kappa_tf], dim=-1),
+            torch.stack([kappa_tf, tau_tf], dim=-1)
+            ], dim=-2)
+
+        self._crossing_tf_matrix = torch.block_diag(edge_up_crossing, *crossing_elements, edge_dw_crossing)
+    
+    def forward(self, x_matrix):
+        return torch.mm(self._crossing_tf_matrix, x_matrix)
---
+++ 20240507_traking_unitary_efficent/20240525_3_run/run_HPC_hyp7.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+#PBS -N neuropuls
+#PBS -l nodes=1:ppn=48
+#PBS -l walltime=48:00:00
+module swap cluster/doduo
+module load Anaconda3/2023.03-1
+source activate /data/gent/vo/000/gvo00005/vsc47558/env/torch_HPC2v
+cd /data/gent/vo/000/gvo00005/vsc47558
+
+mpirun -n 48 python ./20240525_3_run/code/tracking_multiCPU.py ./20240525_3_run/hyp7.json
