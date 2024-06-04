a38282aeb80d7e7c796e6beccc271fb92aa60ee9
SBern1w <marchesin.federico@gmail.com>
2024-06-04 16:39:17 +0200

    Add of the fidelity program

---
+++ 20240601_traking_new_runs/test_run_cossim_myPC/run_python_myPC.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+cd 20240601_traking_new_runs/test_run_cossim_myPC
+
+# Read the JSON file
+json_file="hyp.json"
+
+# Extract parameters for the current array job
+run_index=0
+
+n_inputs=$(jq ".runs[$run_index].n_inputs" $json_file)
+i_loss=$(jq ".runs[$run_index].i_loss" $json_file)
+imbalance=$(jq ".runs[$run_index].imbalance" $json_file)
+folder_path=$(jq -r ".runs[$run_index].folder_path" $json_file)  # -r flag to output raw string without quotes
+
+mpirun -n 12 python ./code/tracking_multiCPU.py --n_inputs $n_inputs --i_loss $i_loss --imbalance $imbalance --folder_path $folder_path
